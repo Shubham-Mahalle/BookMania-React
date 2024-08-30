22ee3726418ef65916ff8e0ad2860c2601ec5864
Shubham-Mahalle <shubhammahalle691@gmail.com>
2024-08-30 21:18:42 +0530

    Add Files

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from "../Navbar/Navbar";
+import SearchForm from "../SearchForm/SearchForm";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className='holder'>
+          <Navbar />
+    </div>
+  )
+}
+
+export default Header
